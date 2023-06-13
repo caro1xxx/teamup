bcc4c7ee8652dc149f042db2cdea4ff723bc1cc2
caro1xxx <caro1xxxhv@gmail.com>
2023-06-13 23:05:17 +0800

    inital project

---
+++ teamup/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MediaNavbarItem from "./components/MediaNavbarItem";
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <div>
+      <MediaNavbarItem />
+    </div>
+  );
+};
+
+export default App;
